3c9ba0ef0c4f3b5607c724488d9106423196d48f
hyeji <laon1551@naver.com>
2022-02-04 14:17:19 +0900

    테스트 코드 넣고 시작

---
+++ src/main/java/com/sparta/myspring03/Myspring03Application.java
@@ -0,0 +1,13 @@
+package com.sparta.myspring03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myspring03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Myspring03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/myspring03/Myspring03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.myspring03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myspring03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
