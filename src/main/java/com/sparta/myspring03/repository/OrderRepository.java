e89686789cca9ec86cc476cd3cdf59bac5aa43b2
hyeji <laon1551@naver.com>
2022-02-08 14:01:33 +0900

    양방향 버전으로 다시 시작

--- src/main/java/com/sparta/myspring03/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.myspring03.repository;
-
-import com.sparta.myspring03.model.Ordering;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Ordering,Long> {
-}
